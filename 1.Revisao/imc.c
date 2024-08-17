b3eff7f871f25a881dd14a0ca0f77dcc9911b4b8
moisesfilhosenai <moises.filho@docente.senai.br>
2024-08-17 14:24:48 -0300

    Resolução exercício IMC

---
+++ 1.Revisao/imc.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    char nome[10];
+    int peso = 0;
+    float altura = 0.0;
+
+    printf("Qual seu nome? ");
+    scanf("%s", nome);
+
+    printf("Qual seu peso? ");
+    scanf("%i", &peso);
+
+    printf("Qual sua altura? ");
+    scanf("%f", &altura);
+
+    float imc = peso / (altura * altura);
+
+    if (imc < 18.5)
+    {
+        printf("Seu IMC eh: %.2f, classificado como Baixo Peso", imc);
+    } 
+    else if (imc >= 18.5 && imc <= 24.9) 
+    {
+        printf("Seu IMC eh: %.2f, classificado como Normal", imc);
+    }
+    else if (imc >= 25 && imc <= 29.9) 
+    {
+        printf("Seu IMC eh: %.2f, classificado como Sobrepeso", imc);
+    }
+    else if (imc >= 30 && imc <= 34.9)
+    {
+        printf("Seu IMC eh: %.2f, classificado como Obesidadade grau I", imc);
+    }
+    else if (imc >= 35 && 39.9)
+    {
+        printf("Seu IMC eh: %.2f, classificado como Obesidadade grau II", imc);
+    }
+    else if (imc > 39.9)
+    {
+        printf("Seu IMC eh: %.2f, classificado como Obesidadade grau III", imc);
+    }
+
+    return 0;
+}
